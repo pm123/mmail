adf99420677c86a394d0d18c86e4a8f2ff690b39
pm123 <879796120@qq.com>
2017-08-04 14:04:32 +0800

    payment

--- src/page/payment/index.js
+++ src/page/payment/index.js
@@ -2,7 +2,59 @@
 * @Author: Dell
 * @Date:   2017-08-04 13:04:00
 * @Last Modified by:   Dell
-* @Last Modified time: 2017-08-04 13:04:00
+* @Last Modified time: 2017-08-04 13:51:57
 */
 
 'use strict';
+require('./index.css');
+require('page/common/header/index.js');
+require('page/common/nav/index.js');
+var _mm = require('util/mm.js');
+var _payment = require('service/payment-service.js');
+var templateIndex = require('./index.string')
+
+//page 逻辑部分
+var page = {
+	data: {
+		orderNumber: _mm.getUrlparam('orderNumber')
+	},
+	init: function(){
+		this.onLoad();
+	},
+	onLoad: function(){
+		//加载订单详情
+		this.loadPaymentInfo();
+	},
+	//加载订单详情
+	loadPaymentInfo: function(){
+		var _this = this,
+			paymentHtml = '',
+			$pageWrap = $('.page-wrap');
+		$pageWrap.html('<div class="loading"></div>');
+		_payment.getPaymentInfo(_this.data.orderNumber, function(res){
+			//渲染订单
+			paymentHtml = _mm.renderHtml(templateIndex,res);
+			$pageWrap.html(paymentHtml);
+			_this.listenOrderStatus();
+		},function(errMsg){
+			$pageWrap.html('<p class="err-tip">'+ errMsg +'</p>');
+		});
+	},
+	//监听订单状态
+	listenOrderStatus: function(){
+		var _this = this;
+		this.paymetTimer = window.setInterval(function(){
+			_payment.getPaymentStatus(_this.data.orderNumber, function(res){
+				if(res == true){
+					window.location.href = 
+						'./result.html?type=payment&orderNumber=' + 
+						_this.data.orderNumber;
+				}
+			})
+		}, 5000)
+	}
+
+}
+$(function(){
+	page.init();
+});
--- src/page/result/index.js
+++ src/page/result/index.js
@@ -2,7 +2,7 @@
 * @Author: Dell
 * @Date:   2017-07-22 14:59:55
 * @Last Modified by:   Dell
-* @Last Modified time: 2017-07-22 15:45:25
+* @Last Modified time: 2017-08-04 13:59:03
 */
 
 'use strict';
@@ -14,7 +14,11 @@ var _mm = require('util/mm.js');
 $(function(){
 	var type = _mm.getUrlparam('type') || 'default' ,
 		$element = $('.' + type + '-success');
-
+	if(type === 'payment'){
+		var orderNumber = _mm.getUrlparam('orderNumber'),
+			$orderNumber = $element.find('.order-number');
+		$orderNumber.attr('href',$orderNumber.attr('href') + orderNumber);
+	}
 	//显示对应的提示元素
 	$element.show();
 	
